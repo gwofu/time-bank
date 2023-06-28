e02485fe6052fbd33d68d0114640390cbd2c73f2
gwofu <gwofu@yahoo.com>
2023-06-28 19:51:22 +0000

    Enhancement

--- main.py
+++ main.py
@@ -2,42 +2,37 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    "id": 1,
-    "title": "Data Analyst",
-    "location": "Chicago, IL",
-    "salary": "72000"
-  },
-  {
-    "id": 2,
-    "title": "Software Engineer",
-    "location": "Chicago, IL",
-    "salary": "102000"
-  },
-  {
-    "id": 3,
-    "title": "Backend Developer",
-    "location": "Chicago, IL",
-    "salary": "82000"
-  },
-  {
-    "id": 4,
-    "title": "Frontend Developer",
-    "location": "Chicago, IL",
-    "salary": "72000"
-  },
-  {
-    "id": 5,
-    "title": "Data Developer",
-    "location": "Hoffman Estates, IL"
-  }    
-]
+JOBS = [{
+  "id": 1,
+  "title": "Data Analyst",
+  "location": "Chicago, IL",
+  "salary": "72000"
+}, {
+  "id": 2,
+  "title": "Software Engineer",
+  "location": "Chicago, IL",
+  "salary": "102000"
+}, {
+  "id": 3,
+  "title": "Backend Developer",
+  "location": "Chicago, IL",
+  "salary": "82000"
+}, {
+  "id": 4,
+  "title": "Frontend Developer",
+  "location": "Chicago, IL",
+  "salary": "72000"
+}, {
+  "id": 5,
+  "title": "Data Developer",
+  "location": "Hoffman Estates, IL"
+}]
 
 
 @app.route("/")
 def index():
   return render_template("home.html", jobs=JOBS, company_name="Gwo Fu")
+
 
 @app.route("/api/jobs")
 def list_jobs():
@@ -50,5 +45,4 @@ def user():
 
 
 if __name__ == "__main__":
-  print("I am inside the if now")
   app.run(host="0.0.0.0", debug=True)
